1e5a206b78d3fa186007abc4736c848eddc49c6b
Chetan Chavan <115619207+Chaitanya0202@users.noreply.github.com>
2024-06-04 13:14:02 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -86,7 +86,7 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/1fUXcyh4HCDkoxhTXs5IfxB_UzJl6yoBQ/view?usp=drivesdk"
+          href="https://drive.google.com/file/d/1tmEVz4FQgyiMtYoiLe4Z2P0FrO8_kFse/view?usp=drivesdk"
           target="_blank"
           download
         >
